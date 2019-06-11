ebedf8dc64fede9a1aee440ac693a7397aa653fa
Natalie Dodd <natdodd@wkmde8677618.global.publicisgroupe.net>
2019-06-11 15:00:04 +0200

    add nav svg icons

--- src/components/Nav/styled.js
+++ src/components/Nav/styled.js
@@ -1,20 +1,10 @@
 import styled from "styled-components";
 
 export const StyledNav = styled.nav`
-  background: rgb(79, 126, 229);
   display: flex;
   justify-content: space-between;
-  padding: 2vmin;
+  padding: 20px;
 
   .navigation-link-container {
-  }
-
-  .nav-link {
-    color: black;
-  }
-
-  .nav-link:active,
-  .nav-link:visited {
-    color: lightblue;
   }
 `;
